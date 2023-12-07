ca7a51f33ef42074b7ccea53a2617b91254c1208
md-mahin-islam-mahi <wdmahin.mhm@gmail.com>
2023-12-07 18:48:31 +0600

    installed basic components library, daisy ui and keep-react

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import keepPreset from "keep-react/src/keep-preset.js";
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: ["light"],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui"), keepPreset],
+}
