10dd98b275b5ffb4a8f7a2aa0d8a8b1bbc3fc901
md-mahin-islam-mahi <wdmahin.mhm@gmail.com>
2023-12-08 15:35:45 +0600

    changed navbar

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            <h1>Hello to home</h1>
+           <Banner />
         </div>
     );
 };
